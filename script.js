dab0129a6204a0f7a4d518f32f1fc29e48974a16
slowhnix <115403305+slowhnix@users.noreply.github.com>
2023-03-08 19:06:36 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+const chatbotMessages = document.getElementById('chatbot-messages');
+const userInput = document.getElementById('user-input');
+const sendButton = document.getElementById('send-button');
+
+// Ajoute un message à la conversation
+function addMessage(text, sender) {
+  const messageElement = document.createElement('div');
+  messageElement.className = `chatbot-message chatbot-message-${sender}`;
+  messageElement.innerText = text;
+  chatbotMessages.appendChild(messageElement);
+  // Scroll vers le bas pour voir le dernier message
+  chatbotMessages.scrollTop = chatbotMessages.scrollHeight;
+}
+
+// Envoyer une requête à l'API de Wikipédia et afficher les résultats
+async function sendRequest(query) {
+  try {
+    const response = await fetch(`https://api.allorigins.win/get?url=${encodeURIComponent(`https://fr.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&titles=${query}&exintro=true&explaintext=true`)}`);
+    const data = await response.json();
+    const pageData = JSON.parse(data.contents);
+    const pageId = Object.keys(pageData.query.pages)[0];
+    const extract = pageData.query.pages[pageId].extract;
+    addMessage(extract, 'bot');
+  } catch (error) {
+    addMessage("Désolé, je n'ai pas trouvé d'informations à ce sujet.", 'bot');
+  }
+}
+
+// Gérer l'événement lorsqu'un message est envoyé
+function handleSend() {
+  const text = userInput.value;
+  if (!text) {
+    return;
+  }
+  addMessage(text, 'user');
+  sendRequest(text);
+  userInput.value = '';
+}
+
+// Associer la fonction handleSend à l'événement click du bouton d'envoi
+sendButton.addEventListener('click', handleSend);
+
+// Associer la fonction handleSend à l'événement submit du formulaire
+document.querySelector('form').addEventListener('submit', function (event) {
+  event.preventDefault();
+  handleSend();
+});
